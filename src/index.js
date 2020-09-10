ca2ebf66edafe473fe330b91ae3bc0b9e4e93d05
walid horchani <walid.horchani@oyez.fr>
2020-09-10 10:59:48 +0100

    mutation createPost

--- src/index.js
+++ src/index.js
@@ -1,29 +1,69 @@
-const {GraphQLServer} = require('graphql-yoga');
-const {randomBytes} = require('crypto');
+const { GraphQLServer } = require('graphql-yoga');
+const { randomBytes } = require('crypto');
 
 // fake data
 const users = [
-  {id: "1", name: "walid", job: 'juoueur', address:"ca", email: 'walidhorchani@gmail.com'},
-  {id: "2", name: "mayssa", job: 'prof', address:"est", email: 'horchani@gmail.com'},
-  {id: "3", name: "arwa", job: 'teacher', address:"css", email: 'tout@gmail.com'},
-  {id: "4", name: "mohamed", job: 'avion', address:"ess", email: 'doq@gmail.com'},
+  {
+    id: '1',
+    name: 'walid',
+    job: 'juoueur',
+    address: 'ca',
+    email: 'walidhorchani@gmail.com',
+  },
+  {
+    id: '2',
+    name: 'Aya',
+    job: 'prof',
+    address: 'est',
+    email: 'horchani@gmail.com',
+  },
+  {
+    id: '3',
+    name: 'arwa',
+    job: 'teacher',
+    address: 'css',
+    email: 'tout@gmail.com',
+  },
+  {
+    id: '4',
+    name: 'mohamed',
+    job: 'avion',
+    address: 'ess',
+    email: 'doq@gmail.com',
+  },
 ];
 
 const pts = [
-  {id :"1", title: "good food", body : "this is a greate foods hello", published: true, author: "1"},
-  {id :"2", title: "plage bizete", body : "fait une randone ensuite camping au palge biz", published: true, author: "2"},
-  {id :"3", title: "parapente", body : "ail du ciele magnifique experirnece", published: true, author: "1"},
-
+  {
+    id: '1',
+    title: 'good food',
+    body: 'this is a greate foods hello',
+    published: true,
+    author: '1',
+  },
+  {
+    id: '2',
+    title: 'plage bizete',
+    body: 'fait une randone ensuite camping au palge biz',
+    published: true,
+    author: '2',
+  },
+  {
+    id: '3',
+    title: 'parapente',
+    body: 'ail du ciele magnifique experirnece',
+    published: true,
+    author: '1',
+  },
 ];
 
 const coms = [
-  {id: "1", content: "my first comment", author: "2", post: "1"},
-  {id: "2", content: "my 2eme comment", author: "2", post: "1"},
-  {id: "3", content: "my troisieme comment", author: "1", post: "2"},
-]
+  { id: '1', content: 'my first comment', author: '2', post: '1' },
+  { id: '2', content: 'my 2eme comment', author: '2', post: '1' },
+  { id: '3', content: 'my troisieme comment', author: '1', post: '2' },
+];
 
 // Type Query and Mutation
-
 
 const typeDefs = `
 type Query {
@@ -36,6 +76,7 @@ type Query {
 }
 type Mutation {
   createUser(name: String!, email: String!, age: Int): User!
+  createPost(title: String!, body : String! , published: Boolean, author: String): Post!
 }
 
 type Comment {
@@ -62,19 +103,15 @@ type Post {
   published: Boolean!
   author: User
 }
-`
-
+`;
 
 const resolvers = {
-  Query:{
-
+  Query: {
     comment(parent, args, ctx, info) {
-       return coms.find((comment) => {
+      return coms.find((comment) => {
         console.log('coooooo::', args.id);
-        return comment.id * 1  == args.id * 1;
-       }
-          
-       )
+        return comment.id * 1 == args.id * 1;
+      });
       //return coms[args.id];
     },
 
@@ -82,92 +119,111 @@ const resolvers = {
       return coms;
     },
 
-
-    posts(parent, args, ctx, info){
-      if (!args.query){
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
         return pts;
       }
       return pts.filter((post) => {
-        return post.title.toLowerCase().includes(args.query.toLowerCase()) || post.body.toLowerCase().includes(args.query.toLowerCase())
-      })
-
+        return (
+          post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+          post.body.toLowerCase().includes(args.query.toLowerCase())
+        );
+      });
     },
 
     users(parent, args, ctx, info) {
-      if (!args.query){
+      if (!args.query) {
         return users;
       }
-      return users.filter(user => {
+      return users.filter((user) => {
         return user.name.toLowerCase().includes(args.query.toLowerCase());
       });
     },
 
-    me () {
+    me() {
       return {
         id: '123456789',
         name: 'walidos',
         job: 'devdouv',
-        address: 'badezvieald cote mer rue 12'
-      }
+        address: 'badezvieald cote mer rue 12',
+      };
     },
 
-    post () {
+    post() {
       return {
-        id: "151515454",
+        id: '151515454',
         title: 'learning graphql',
         body: 'formation gratuit  pour apprendre graphql ',
         published: true,
-      }
-    }
+      };
+    },
   },
 
   Mutation: {
-    createUser(parent, args, ctx, info){
-      const emailTaken = users.some(user => user.email === args.email); 
-      if(emailTaken) {
+    createUser(parent, args, ctx, info) {
+      const emailTaken = users.some((user) => user.email === args.email);
+      if (emailTaken) {
         throw Error('email taken !');
       }
       const newUser = {
-        id : randomBytes(4).toString('hex'),
+        id: randomBytes(4).toString('hex'),
         name: args.name,
-        job: args.job, 
-        address: args.adress, 
-        email: args.email
-      }
+        job: args.job,
+        address: args.adress,
+        email: args.email,
+      };
       users.push(newUser);
-      
+
       return newUser;
-    }
+    },
+    createPost(parent, args, ctx, info) {
+      //check if user exist
+      const userExist = users.some((user) => user.id === args.author);
+      if (!userExist) {
+        throw new Error('user not found !');
+      }
+
+      const post = {
+        id: randomBytes(4).toString('hex'),
+        title: args.title,
+        body: args.body,
+        published: args.published || true,
+        author: args.author,
+      };
+
+      pts.push(post);
+
+      return post;
+    },
   },
 
   Post: {
     author(parent, args, ctx, info) {
-      console.log('nu par is;;;', parent)
-      return users.find(user =>  parent.id === user.id)
-    }
+      console.log('nu par is;;;', parent);
+      return users.find((user) => parent.author === user.id);
+    },
   },
   Comment: {
     author(parent, args, ctx, info) {
       console.log('author id is :::', parent.author);
-      return users.find(user => user.id === parent.author);
+      return users.find((user) => user.id === parent.author);
     },
     post(parent, args, ctx, info) {
-      return pts.find(post => post.id == parent.post)
-    }
+      return pts.find((post) => post.id == parent.post);
+    },
   },
   User: {
-    comments (parent, args, ctx, info){
-      return coms.filter(comment => comment.author == parent.id)
-    }
-  }
-}
+    comments(parent, args, ctx, info) {
+      return coms.filter((comment) => comment.author == parent.id);
+    },
+  },
+};
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
-}) 
+});
 
-server.start(()=> {
-  console.log('server lance avec succes...')
-})
-
+server.start(() => {
+  console.log('server lance avec succes...');
+});
